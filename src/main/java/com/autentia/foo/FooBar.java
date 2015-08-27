2c171353ea6648c3bfa7abe9afc88fcf485c8d1d
arihandler <arihandler@autentia>
2015-08-27 15:10:41 +0200

    inital commit

---
+++ src/main/java/com/autentia/foo/FooBar.java
@@ -0,0 +1,14 @@
+package com.autentia.foo;
+
+public class FooBar {
+
+    private String greetings = "Hello World!";
+
+    public FooBar() {
+    }
+
+    public String greet() {
+        return greetings;
+    }
+
+}
